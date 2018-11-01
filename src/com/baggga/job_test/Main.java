ea3de0e9f5556a86b577bc56dac1de7723517853
baggga <ursa116@gmail.com>
2018-11-01 23:52:30 +0300

    Set up docker.

---
+++ src/com/baggga/job_test/Main.java
@@ -0,0 +1,7 @@
+package com.baggga.job_test;
+
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Test docker!");
+    }
+}
--- src/com/baggga/job_test/controller/VisitInfoController.java
+++ src/com/baggga/job_test/controller/VisitInfoController.java
@@ -1,4 +1,23 @@
 package com.baggga.job_test.controller;
 
+import com.baggga.job_test.bean.VisitInfo;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+
+@Controller
+@RequestMapping("/visit-info")
 public class VisitInfoController {
+    @RequestMapping(value = "create", method = RequestMethod.POST)
+    public String createVisitInfo(@ModelAttribute VisitInfo visitInfo) {
+        return null;
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String getVisitInfo(Model model) {
+        return null;
+    }
 }
